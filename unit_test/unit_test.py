2d264b3fcd1c902f6e9143bfa47226fbd7585944
VHkrishnan0591 <harikrishnanv0591@gmail.com>
2025-06-01 18:10:44 +0200

    Deployment Changes with github actions with coreect file path for the unit test.py file whenn reading the data and model

--- unit_test/unit_test.py
+++ unit_test/unit_test.py
@@ -1,12 +1,16 @@
 import pickle
 import pandas as pd
 import sys
+import os
 from src.logging.logger import logger
 from sklearn.metrics import accuracy_score
-test_data = pd.read_csv(r'unit_test\unit_test.csv')
+base_dir = os.path.dirname(__file__)
+csv_path = os.path.join(base_dir, 'unit_test.csv')
+model_path = os.path.join(base_dir, '..', 'artifacts', 'model_directory', 'best_model.pkl') 
+test_data = pd.read_csv(csv_path) # r'unit_test\unit_test.csv'
 X_test = test_data[:5].drop(columns='Result',axis=1)
 y_test = test_data[:5]['Result']
-with open(r'artifacts\model_directory\best_model.pkl', 'rb') as file:
+with open(model_path, 'rb') as file: # r'artifacts\model_directory\best_model.pkl'
             loaded_model = pickle.load(file)
 y_pred = loaded_model.predict(X_test)
 model_accuracy = (accuracy_score(y_test, y_pred))
